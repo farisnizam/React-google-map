4c829aab07e5de0ca8f2261f74232b037de531b1
AbduJaine <abdujaine@ifca.com.my>
2022-09-30 12:24:34 +0800

    add Title, InfoWindow

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -3,13 +3,29 @@ import { useEffect, useState } from "react";
 function Marker({
   position,
   map,
+  title,
+  contenttt,
 }: {
   position: google.maps.LatLngLiteral;
   map?: google.maps.Map;
+  title: any;
+  contenttt: any;
 }) {
   console.log({ position });
 
   const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contenttt,
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });
 
   useEffect(() => {
     setMarker(new google.maps.Marker({}));
@@ -18,6 +34,7 @@ function Marker({
   if (marker) {
     marker.setMap(map);
     marker.setPosition(position);
+    marker.setTitle(title);
   }
   return null;
 }
